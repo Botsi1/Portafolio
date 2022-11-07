b0de1d3c8d4b29383f2c57523fd8dba7bb8309b6
Botsi1 <bmoreiro@gmail.com>
2022-11-07 16:42:11 -0300

    casi terminado 2

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import "./About.css"
+import java from "../../Media/Java.png"
+import node from "../../Media/Node.png"
+import react from "../../Media/React.png"
+import css from  "../../Media/css.png"
+import html from  "../../Media/html.png"
 
 const About = () => {
 
@@ -7,11 +12,25 @@ const About = () => {
         <div className='about-container'>
             <div className='about-desc'>
                 <h3>Let me tell you something about ame</h3>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis
-                    provident inventore quod voluptates sequi. Quis, necessitatibus omnis
-                    iusto perferendis dolores voluptate et dolorum inventore autem nemo,
-                    quos eveniet magni nesciunt.
-                </p>
+                <p> I am a full stack developer, since I was a child I like everything that has to do with science and technology, I am a student of electronic engineering, I am currently in my third year.
+                    I am from Argentina and with respect to programming I have knowledge in different technologies:</p>
+                <picture className='container-picture'>
+                    <div className='picture'><img src={java} alt="JavaScript"></img></div>
+                    
+                    <div className='picture'><img src={node} alt="node"></img></div>
+                    
+                    <div className='picture'><img src={react} alt="react"></img></div>
+                    
+                </picture>
+
+                <picture className='container-picture2'>
+                    <div className='picture'><img src={css} alt="css"></img></div>
+                    
+                    <div className='picture'><img src={html} alt="html"></img></div>
+                    
+                    
+                    
+                </picture>
 
 
 
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import "./Footer.css"
+import git from "../../Media/Git.png"
 
 const Footer = () => {
-  return (
-    <footer className='footer'>
-        <div className='footer-info'>
-            <h1>Bautista</h1>
-            <p>Argentina</p>
-        </div>
-        <div className='footer-contact'>
-            <h3>Contact me</h3>
-            <p>And let's get down to work</p>
-        </div>
-        <div className=' footer-sns'>
-            <div className='design-by'> Design By Botsi</div>
-            <div className='sns-links'>
-                <a href="https://www.linkedin.com/in/bautista-moreiro/" target="_blank" rel='noreferrer'>
-                    <i className='fab fa-linkedin linkedin'></i>
-                </a>
+    return (
+        <footer className='footer'>
+            <div className='footer-info'>
+                <h1>Bautista</h1>
+                <p>Argentina</p>
             </div>
-        </div>
+            <div className='footer-contact'>
+                <h3>Contact me</h3>
+                <p>And let's get down to work</p>
+            </div>
+            <div className=' footer-sns'>
+                <div className='design-by'> Design By Botsi</div>
+                <div className='sns-links'>
+                    <a href="https://www.linkedin.com/in/bautista-moreiro/" target="_blank" rel='noreferrer'>
+                        <i className='fab fa-linkedin linkedin'></i>
+                    </a>
+                    <a href="https://github.com/Botsi1" target="_blank" rel='noreferrer'>
+                        <i className='fab fa-github github'></i>
+                    </a>
+                </div>
+            </div>
 
-    </footer>
- 
-  )
+        </footer>
+
+    )
 }
 
 export default Footer
